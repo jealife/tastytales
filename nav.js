58990037624f5df252c7cb59bbbc847a10fe0d42
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-03-25 01:28:21 +0100

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,42 @@
+const hamburgerToggler = document.querySelector(".hamburger")
+const navLinksContainer = document.querySelector(".navlinks-container");
+
+const toggleNav = () => {
+  hamburgerToggler.classList.toggle("open")
+
+  const ariaToggle = hamburgerToggler.getAttribute("aria-expanded") === "true" ?  "false" : "true";
+  hamburgerToggler.setAttribute("aria-expanded", ariaToggle)
+
+  navLinksContainer.classList.toggle("open")
+}
+hamburgerToggler.addEventListener("click", toggleNav)
+
+new ResizeObserver(entries => {
+  if(entries[0].contentRect.width <= 900){
+    navLinksContainer.style.transition = "transform 0.3s ease-out"
+  } else {
+    navLinksContainer.style.transition = "none"
+  }
+}).observe(document.body)
+
+//Record how many pixels are scrolled vertically
+let scrollpos = window.scrollY
+
+const header = document.querySelector("nav")
+
+//How many pixels you need to scroll to add a class. You can change the value
+const scrollChange = 1
+
+//The function that will add the class
+const add_class_on_scroll = () => header.classList.add("fixed")
+
+//Tracking a "scroll" event
+window.addEventListener('scroll', function() { 
+  scrollpos = window.scrollY;
+
+//If we scrolled more than we specified in the scrollChange variable, the add class function is executed
+  if (scrollpos >= scrollChange) { add_class_on_scroll() }
+  else{
+    header.classList.remove('fixed');
+  }
+})
